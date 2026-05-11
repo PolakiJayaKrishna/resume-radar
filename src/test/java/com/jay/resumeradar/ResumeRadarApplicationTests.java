a8839458f1ee17300488736047ce012abb986f53
Polaki Jaya Krishna <polakijayakrishna@gmail.com>
2026-05-11 16:52:45 +0530

    Initial setup: Spring Boot 3.3.6 + Java 21 + MySQL dependencies

---
+++ src/test/java/com/jay/resumeradar/ResumeRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.resumeradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeRadarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
